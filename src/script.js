f6dad8bf2750edb5d610d8e6bfd4cab9ab69801d
Ameer <ameerapex496@gmail.com>
2023-11-29 19:48:21 +0100

    added function and fontaswomse

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
-console.log("hallo world");
+var body = document.body;
+var slides = document.querySelectorAll(".slide");
+var leftBtn = document.getElementById("left");
+var rightBtn = document.getElementById("right");
+var activeSlide = 0;
+if (rightBtn && leftBtn) {
+    rightBtn.addEventListener("click", function () {
+        activeSlide++;
+        if (activeSlide > slides.length - 1) {
+            activeSlide = 0;
+        }
+        setBgToBody();
+        setActiveSlide();
+    });
+    leftBtn.addEventListener("click", function () {
+        activeSlide--;
+        if (activeSlide < 0) {
+            activeSlide = slides.length - 1;
+        }
+        setBgToBody();
+        setActiveSlide();
+    });
+}
+setBgToBody();
+function setBgToBody() {
+    if (body) {
+        body.style.backgroundImage =
+            slides[activeSlide].style.backgroundImage || "";
+    }
+}
+function setActiveSlide() {
+    slides.forEach(function (slide) { return slide.classList.remove("active"); });
+    if (slides[activeSlide]) {
+        slides[activeSlide].classList.add("active");
+    }
+}
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,47 @@
-console.log("hallo world");
+const body: HTMLElement | null = document.body;
+const slides: NodeListOf<Element> = document.querySelectorAll(".slide");
+const leftBtn: HTMLElement | null = document.getElementById("left");
+const rightBtn: HTMLElement | null = document.getElementById("right");
+
+let activeSlide: number = 0;
+
+if (rightBtn && leftBtn) {
+  rightBtn.addEventListener("click", () => {
+    activeSlide++;
+
+    if (activeSlide > slides.length - 1) {
+      activeSlide = 0;
+    }
+
+    setBgToBody();
+    setActiveSlide();
+  });
+
+  leftBtn.addEventListener("click", () => {
+    activeSlide--;
+
+    if (activeSlide < 0) {
+      activeSlide = slides.length - 1;
+    }
+
+    setBgToBody();
+    setActiveSlide();
+  });
+}
+
+setBgToBody();
+
+function setBgToBody() {
+  if (body) {
+    body.style.backgroundImage =
+      slides[activeSlide].style.backgroundImage || "";
+  }
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+
+  if (slides[activeSlide]) {
+    slides[activeSlide].classList.add("active");
+  }
+}
